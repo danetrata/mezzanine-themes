a79efbcacf8f9299efce024bc7a1e84f8b629d92
Daniel Etrata <danetrata@gmail.com>
2020-04-27 13:00:46 -0500

    make this installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mezzanine-themes',
+    version='1.0.0',
+    url='https://github.com/thecodinghouse/mezzanine-themes.git',
+    author='thecodinghouse',
+    author_email='',
+    description='Mezzanine themes',
+    packages=find_packages(),    
+    # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
